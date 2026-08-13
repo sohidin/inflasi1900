55083005687f9fc2a7cb94a9049da74ac55284b7
sohidin <sohidin12@gmail.com>
2026-08-13 11:37:18 +0700

    Delete js/config.js

--- js/config.js
+++
@@ -1,9 +0,0 @@
-window.APP_CONFIG = {
-  // GANTI dengan URL Web App Apps Script setelah deploy.
-  API_URL: "https://script.google.com/macros/s/AKfycbyMl-9Sjxz9AZSatXWTl_dEC4uXQYfqq4LtNgND2TZOVqXssxgTum6X2iPHS50F-elb/exec",
-
-  // Spreadsheet sumber.
-  SPREADSHEET_ID: "1i-bg6Jd2bNiJhwB90UjrJZs_wSaUEenYydTcLgOKnNI",
-
-  APP_NAME: "Dashboard Inflasi 1900"
-};
